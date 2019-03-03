a4d51f406d15442b06fcff1e3e6ccc923673aa7b
tibo456 <47974625+tibo456@users.noreply.github.com>
2019-03-03 18:41:06 +0100

    Add files via upload

--- html/private/security/anti_SQL_injection.php
+++ html/private/security/anti_SQL_injection.php
@@ -3,6 +3,7 @@ auteur:Thibaud Jacquelin
 10.02.2019
 modification:
 Thibaud JACQUELIN, 24-02-2019, rajout des echapatoires en SQL et de la protection des injections html
+Thibaud JACQUELIN, 03-03-2019, rajout du filtrage des balises
 -->
 
 <?php
@@ -11,6 +12,7 @@ foreach($_SESSION as $cle => $valeur)
 {
 
 	$_SESSION[$cle]=htmlspecialchars($valeur);
+	$_SESSION[$cle]=filter_var($valeur, FILTER_SANITIZE_STRING);//filtre les balises
 
 }
 
@@ -19,6 +21,7 @@ foreach($_POST as $cle => $valeur)
 {
 
 	$_POST[$cle]=htmlspecialchars($valeur);
+	$_POST[$cle]=filter_var($valeur, FILTER_SANITIZE_STRING);
 
 }
 
@@ -27,6 +30,6 @@ foreach($_GET as $cle => $valeur)
 {
 
 	$_GET[$cle]=htmlspecialchars($valeur);
-
+	$_POST[$cle]=filter_var($valeur, FILTER_SANITIZE_STRING);
 }
 
