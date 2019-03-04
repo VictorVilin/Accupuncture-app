dd4a2146c06075f1124d4089aa2c58b298573c71
VictorVilin <47083451+VictorVilin@users.noreply.github.com>
2019-03-04 10:07:34 +0100

    Update connectionbar.php

--- html/compte/connectionbar.php
+++ html/compte/connectionbar.php
@@ -21,14 +21,14 @@ if(isset($_SESSION['prenom']))
 	<form method=post action="index.php?action=connection">
 	<fieldset>
 
-	  <label for="email">identifiant:</label>
+	  <label for="email">Identifiant :</label>
 	  <input type="email" name="email" id="email" placeholder="email" autocomplete="email"/>
 
-	  <label for="password">mdp:</label>
+	  <label for="password">Mot de Passe :</label>
 	  <input type="password" name="password" id="password" placeholder="mot de passe"/>
 
 	  <input type="submit" name="connexion" value="connexion"/>
-	  <a href="index.php?action=inscriptionpage" title="inscrivez vous ici !">inscription</a>
+	  <a href="index.php?action=inscriptionpage" title="Inscrivez vous ici !">Inscription</a>
 	</fieldset>
 	</form>
 <?php
