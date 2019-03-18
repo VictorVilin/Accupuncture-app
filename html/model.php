d36f7afabccbb2e751b15dfee550b6078cf995da
paul69800 <47972711+paul69800@users.noreply.github.com>
2019-03-18 09:34:40 +0100

    Add files via upload

--- html/model.php
+++ html/model.php
@@ -1,6 +1,8 @@
 <!-- affichage commun de toutes les pages du site (header, barre de connection, chat...
 auteur:Thibaud Jacquelin
 09.02.2019
+modification:
+Paul Martinez, 18/03/2019, rajout de la feuille de style pour l'impression
 -->
 
 <!doctype html>
@@ -9,14 +11,14 @@ auteur:Thibaud Jacquelin
   <meta charset="utf-8">
   <title>Wikipuncture</title>
   <link rel="stylesheet" href="CSS/style.css">
+  <link rel="stylesheet" href="CSS/impression.css" media="print">
 </head>
-<header>
     <?php include("header.php"); ?>
-</header>
     <body>
 	<div class=side>
 	<?php include("compte/connectionbar.php"); ?>
 	<?php include("menu.php"); ?>
 	<?php include("chat/chatbar.php"); ?>
 	</div>
-
+    </body>
+</html>
