14cc5115cb739242cca7204ee6b66152be4afe9d
paul69800 <47972711+paul69800@users.noreply.github.com>
2019-03-18 11:20:25 +0100

    Add files via upload

--- html/information.php
+++ html/information.php
@@ -1,6 +1,6 @@
-<!-- page d'accueil, affiche le moteur de recherche
-auteur:Thibaud Jacquelin
-09.02.2019
+<!-- page d'information
+auteur:Paul Martinez
+18/03/2019
 -->
 
 <div class = content>
@@ -8,14 +8,33 @@ auteur:Thibaud Jacquelin
 	<h3> Développements </h3>
 	<p> Voici les differents développements apportés au site web : </p>
 	<ul>
-        	<li>idée 1</li>
-		<li><idée 2/></li>
+        	<li>recherche de pathologie</li>
+		<li>identification par utilisateur</li>
+		<li>nous contacter</li>
+		<li>informations</li>
+		<li>chat en ligne</li>
 	</ul>
 	<h3> Sources: </h3>
 	<ul>
-        	<li>idée 1</li>
-		<li><idée 2/></li>
+        	<li>https://stackoverflow.com/</li>
+		<li>https://openclassrooms.com/fr/</li>
+	</ul>
+	<h3> Developpeurs: </h3>
+	<ul>
+        	<li>Paul Martinez </li>
+		<img src="images/paul2.jpg"> 
+		
+		<li>Victor Vilin</li> 
+		<img src="images/victor.jpg"> 
+
+		<li>Thibault Jacquelin</li>
+		<img src="images/thibault.jpg"> 
+
+		<li>Àngel Calvo </li>
+		<img src="images/angel.jpg"> 
+
 	</ul>
 
 </div>
+
 
